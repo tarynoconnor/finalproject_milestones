96f96889dcd2695a6accb2cfa8ed0dd5064eb290
Taryn O'Connor <tarynoconnor@dhcp-10-250-228-194.harvard.edu>
2020-10-23 03:40:17 -0400

    Milestone #5

--- Milestone_4/app.R
+++ Milestone_4/app.R
@@ -6,7 +6,7 @@ superpac_app = readRDS("superpac.RDS")
 
 # Here is my setup for my model, discussion, and about tabs.
 ui <- navbarPage(
-    "Milestone 4",
+    "Milestone 5",
     tabPanel("Model",
              fluidPage(
                  titlePanel("Individual Contributions to Super PACS with > 
@@ -24,25 +24,19 @@ ui <- navbarPage(
              titlePanel("Discussion"),
              p("Right now, I am still working on narrowing down exactly what 
                data I want to use, and am doing plenty of searches on the Federal
-               Election Commission website. I have also sent a request for access 
-               to OpenSecrets' bulk data. When looking into data sets, I am also
-               taking size into account, as some data sets have millions of 
-               entries (as they are records of all contributions to campaigns or
-               PACS over a given time period). For now, I have focused on a data
-               set I downloaded from the FEC detailing all contributions to Super
-               PACs from 2019-2020. The data in the model shows the distribution
-               of contributions by individuals to super PACs that have received 
-               over 1,000 contributions this year. If I were to continue with something 
-               related to this model, I could potentially look at changes in
-               contributions over the years or compare the highest funded super 
-               PACs with election results. However, one issue I have ran into is 
-               that on the FEC site, you cannot download more than 500,000 contributions 
-               into a single csv file. Instead, you have to download the bulk data, 
-               which raises two issues. First, the file may be too large to load into 
-               RStudio on my computer. Also, the data is saved in a .txt file, which 
-               I'm not sure if it can be formatted into R at all, as when I used 
-               read.delim(), for a small .txt file, there is one column for each 
-               row containing multiple variables separated by |.")),
+               Election Commission website and OpenSecrets. When looking into data 
+               sets, I am also taking size into account, as some data sets have 
+               millions of  entries (as they are records of all contributions to 
+               campaigns or PACS over a given time period). For now, I have focused 
+               on a data set I downloaded from the FEC detailing all contributions 
+               to Super PACs from 2019-2020. The data in the model shows the 
+               distribution  of contributions by individuals to super PACs that 
+               have received over 1,000 contributions this year. If I were to 
+               continue with something related to this model, I could potentially 
+               look at changes in contributions over the years or compare the 
+               highest funded super PACs with election results. However, this would 
+               require downloading bulk data from either the FEC or OpenSecrets, 
+               which would contain millions of entries.")),
     tabPanel("About",
              titlePanel("About"),
              h3("About Me"),
@@ -70,7 +64,10 @@ server <- function(input, output) {
             
 # I used labels = scales::comma to get commas on the y axis
             
-            scale_y_continuous(labels = scales::comma)
+            scale_y_continuous(labels = scales::comma) +
+            theme(text = element_text(size = 20), 
+                  axis.text = element_text(size = 15), 
+                  axis.title = element_text(size = 20,face = "bold"))
     })
 }
 
